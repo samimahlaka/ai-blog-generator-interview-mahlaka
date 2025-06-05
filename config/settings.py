9438835dec069f26b4ed723a377a855ac1729ab1
Mahlaka <samimahlaka@gmail.com>
2025-06-04 20:42:06 -0500

    Configure Django to use PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'yourpassword',  # replace with your actual password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
