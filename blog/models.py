9bca3b96dff8f7eb6abcda0dd5b2f9fce15cdee0
Mahlaka <samimahlaka@gmail.com>
2025-06-05 15:21:20 -0500

    Add Blog model with keyword, blog, and created_at fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    key_word = models.TextField()
+    blog = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.key_word}"
